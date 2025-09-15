91c5d33cf4c84264b1b4a9724c4e91a63fc86e47
Tldev-ai <tutorleapofficial@gmail.com>
2025-09-15 14:15:48 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// App.js - Main React App Component
+
+import React from 'react';
+import VoiceAssistant from './components/VoiceAssistant';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VoiceAssistant />
+    </div>
+  );
+}
+
+export default App;
